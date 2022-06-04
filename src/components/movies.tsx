50cab62f0800eeb7a31198eabfbe7c510e2cb027
Animan Amit <animanamit@wustl.edu>
2022-06-04 13:10:32 +0530

    progress on movie card

--- src/components/movies.tsx
+++ src/components/movies.tsx
@@ -13,21 +13,13 @@ const Movies = () => {
     return (
       <div className='bg-cyan-200 h-screen'>
         <h1>Movies Page</h1>
-        <div className='flex   space-x-2'>
+        <div className='flex space-x-2'>
           {/* {data.results.map((item, index) => (
             <SmallMovieCard data={item} key={index} />
           ))} */}
-          {/* {new Array(5).fill(1).map((_, index) => (
+          {new Array(5).fill(1).map((_, index) => (
             <SmallMovieCard data={data.results[index]} key={index} />
-          ))} */}
-        </div>
-
-        <div className='group'>
-          <img
-            src='https://g.foolcdn.com/image/?url=https%3A%2F%2Fg.foolcdn.com%2Feditorial%2Fimages%2F650245%2Fshiba-inu-shib-doge-dogecoin-token-coin-cryptocurrency-digital-blockchain-technology-invest-getty.jpg&w=700'
-            alt='dog'
-            className='w-40 group-hover:scale-120  duration-200'
-          />
+          ))}
         </div>
       </div>
     )
--- src/components/small-movie-card.jsx
+++ src/components/small-movie-card.jsx
@@ -2,13 +2,24 @@
 import React from 'react'
 
 const SmallMovieCard = ({ data }) => {
+  console.log(data)
   return (
-    <div className='group'>
+    <div className='group overflow-hidden relative'>
       <img
-        src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w300${data.poster_path}`}
         alt='movie poster'
-        className='w-52 h-62 duration-200 group-hover:scale-120'
+        className='h-72 duration-200 group-hover:scale-110'
       />
+      <div
+        className='absolute top-0 bottom-0 right-0 left-0 group-hover:bg-gradient-to-b group-hover:from-transparent group-hover:to-gray-900  group-hover:opacity-20;
+'
+      >
+        <div className='relative h-full  text-center'>
+          <h5 className='bottom-4 left-0 px-2 right-0 absolute h-fit invisible group-hover:visible duration-200 text-white uppercase tracking-tighter'>
+            {data.title}
+          </h5>
+        </div>
+      </div>
     </div>
   )
 }
